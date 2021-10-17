0214b178ce0116f69751bb18a1d7f7902b8ce205
Saish Menon <saishmenon001@gmail.com>
2021-10-17 04:39:20 -0700

    Add Database with read and write

--- app.js
+++ app.js
@@ -1,24 +1,60 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const port = 3000;
-const items = ["Buy Food", "Eat Food", "Cook Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
+const itemSchema = {
+    name: String
+};
 
-app.get("/", function(req, res){
-    
-    let day = date.getDate();
-    res.render("list", {listTitle: day, newListItem: items});
+// Dummy date to be inserted if the database is empty
+const Item = mongoose.model("Item", itemSchema);
+
+const buyCoffee = new Item({
+    name: "Buy Coffee"
 });
 
+const drinkCoffee = new Item({
+    name: "Drink Coffee"
+});
+
+const makeBreakfast = new Item({
+    name: "Make Breakfast"
+});
+
+const defaultItems = [buyCoffee, drinkCoffee, makeBreakfast];
+
+//Home page load
+app.get("/", function(req, res){
+    
+    Item.find({}, function(err, foundItems){
+        if(err){
+            console.log(err);
+        }else if(foundItems.length === 0){
+            Item.insertMany(defaultItems, function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("Successfully added the items to the database.");
+                }
+            });
+        }else{
+            console.log(foundItems);
+            res.render("list", {listTitle: "Today", newListItem: foundItems});
+        }
+    });
+});
+
+// Adding a new To Do item
 app.post("/", function(req, res){
     
     console.log(req.body);
@@ -26,13 +62,23 @@ app.post("/", function(req, res){
     
     if(item !== ""){
         
+        var object = {
+            name: item,
+          };
+
         if(req.body.button === "Work"){
             workItems.push(item);
             console.log("Item pushed to workItems array");
             res.redirect("/work");
         }else{
-            items.push(item);
-            console.log("Item pushed to items array!");
+            Item.create(object, function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("Successfully added " + item + ".");
+                }
+            });
+            
             res.redirect("/");
         }
     }else{
